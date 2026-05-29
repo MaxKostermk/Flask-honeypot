d21e8c1add137067c07133ab7c60090609a6d3d2
MaxKostermk <maxkoster003@gmail.com>
2026-05-29 20:07:23 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 sudo apt update
-sudo apt install -y git python3 python3-venv python3-pip build-essential libssl-dev libffi-dev authbind
+sudo apt-get install python3-pip python3-venv libssl-dev libffi-dev build-essential libpython3-dev python3-minimal authbind
 
-sudo id cowrie >/dev/null 2>&1 || sudo adduser --disabled-password --gecos "" cowrie
-sudo id honeypot >/dev/null 2>&1 || sudo adduser --disabled-password --gecos "" honeypot
+sudo adduser --disabled-password --gecos "" cowrie
+sudo adduser --disabled-password --gecos "" honeypot
+sudo -u cowrie mkdir -p /home/cowrie/cowrie
 sudo mkdir -p /var/log/honeypot
 sudo chown -R honeypot:honeypot /var/log/honeypot
 
@@ -14,24 +15,23 @@ sudo chown -R honeypot:honeypot /opt/honeypot-demo
 sudo -u honeypot python3 -m venv /opt/honeypot-demo/fakepanel/venv
 sudo -u honeypot /opt/honeypot-demo/fakepanel/venv/bin/pip install -r /opt/honeypot-demo/fakepanel/requirements.txt
 
-if [ ! -d /home/cowrie/cowrie ]; then
-  sudo -u cowrie git clone https://github.com/cowrie/cowrie /home/cowrie/cowrie
+sudo -u cowrie bash -c '
+cd /home/cowrie/cowrie
+python3 -m venv cowrie-env
+source cowrie-env/bin/activate
+python -m pip install --upgrade pip
+python -m pip install cowrie
+
+if [ ! -f etc/cowrie.cfg ]; then
+  cowrie init
 fi
+'
 
-sudo -u cowrie python3 -m venv /home/cowrie/cowrie/cowrie-env
-sudo -u cowrie /home/cowrie/cowrie/cowrie-env/bin/pip install --upgrade pip
-sudo -u cowrie /home/cowrie/cowrie/cowrie-env/bin/pip install -r /home/cowrie/cowrie/requirements.txt
-
-if [ ! -f /home/cowrie/cowrie/etc/cowrie.cfg ]; then
-  sudo -u cowrie cp /home/cowrie/cowrie/etc/cowrie.cfg.dist /home/cowrie/cowrie/etc/cowrie.cfg
-fi
-sudo -u cowrie bash -c 'cat >> /home/cowrie/cowrie/etc/cowrie.cfg << EOF
-
-[output_jsonlog]
-enabled = true
-logfile = /home/cowrie/cowrie/var/log/cowrie/cowrie.json
-EOF'
-
+# [output_jsonlog]
+# enabled = true
+# logfile = /home/cowrie/cowrie/var/log/cowrie/cowrie.json
+# EOF'
+#sudo -u cowrie /home/cowrie/cowrie/cowrie-env/bin/cowrie start
 sudo cp systemd/fakepanel.service /etc/systemd/system/fakepanel.service
 sudo cp systemd/cowrie.service /etc/systemd/system/cowrie.service
 sudo systemctl daemon-reload
